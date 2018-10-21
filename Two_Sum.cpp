e8ce0cf6ac386bba8a84647ccc20d4396fddc33a
andychod <andysuper110774@gmail.com>
2018-10-21 20:58:14 +0800

    solve: Two Sum

---
+++ Two_Sum.cpp
@@ -0,0 +1,30 @@
+// Two Sum
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int a, b;
+        int flag = 0;
+        
+        for(int i=0; i<= nums.size()-2; i++)
+        {
+            for(int j=i +1; j<=nums.size()-1; j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    a = i;
+                    b = j;
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 1)
+                break;
+        }
+        
+        vector<int> ans;
+        ans.push_back(a);
+        ans.push_back(b);
+        return ans;
+        
+    }
+};
